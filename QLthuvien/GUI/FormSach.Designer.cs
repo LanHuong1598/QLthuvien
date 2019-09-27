56539c1a0e2c2515a194950ee88cd1540034a186
lanhuong15 <lanhuongitk55lvt@gmail.com>
2019-09-27 08:23:27 +0700

    Update Quan lý sách

--- QLthuvien/GUI/FormSach.Designer.cs
+++ QLthuvien/GUI/FormSach.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormSach";
+            this.ClientSize = new System.Drawing.Size(1165, 563);
+            this.Name = "FormSach";
+            this.Text = "Quản Lý Sách";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
