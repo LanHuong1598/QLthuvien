0e6b9701f74485266725e75649c7e662eea6099e
lanhuong <lanhuong@DESKTOP-EGER9U2>
2019-09-27 08:04:20 +0700

    Add Modun father

---
+++ QLthuvien/DataAccess/ConnectString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLthuvien
+{
+    class ConnectString
+    {
+        public string getConnectionString()
+        {
+            string connectionString = "";
+            try
+            {
+                connectionString = @"Data Source=" + Environment.MachineName + ";Initial Catalog=QuanLyThuVien;Integrated Security=True";
+                using (SqlConnection connect = new SqlConnection(connectionString))
+                {
+
+                    connect.Open();
+                }
+                return connectionString;
+            }
+            catch
+            {
+                string connectionString1 = @"Data Source=DESKTOP-LQQ51RU\SON" + Environment.MachineName + ";" + " Initial Catalog=QuanLyThuVien;Integrated Security=True";
+                return connectionString1;
+            }
+        }
+    }
+}
