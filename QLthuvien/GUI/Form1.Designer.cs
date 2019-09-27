143da92199bcee6d74b588a0f5a6db9da50e4857
lanhuong15 <lanhuongitk55lvt@gmail.com>
2019-09-27 08:16:57 +0700

    Change modun

--- QLthuvien/GUI/Form1.Designer.cs
+++ QLthuvien/GUI/Form1.Designer.cs
@@ -38,27 +38,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(259, 223);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Sách";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(509, 223);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Bạn đọc";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(771, 223);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Mượn trả";
             // 
             // Form1
             // 
