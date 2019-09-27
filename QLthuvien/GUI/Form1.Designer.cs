f5a807db0c2b039db4624fd7015064591d62ac4d
lanhuong15 <lanhuongitk55lvt@gmail.com>
2019-09-27 08:19:48 +0700

    Add fr Sách

--- QLthuvien/GUI/Form1.Designer.cs
+++ QLthuvien/GUI/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sách";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
