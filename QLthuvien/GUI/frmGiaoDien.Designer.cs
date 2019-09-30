3d6aaf7d4b5183184692b58a51074fd28e4405fd
huyhoang189 <vohuyhoang189@gmail.com>
2019-09-30 13:09:26 +0700

    add button for frmGiaoDien

--- QLthuvien/GUI/frmGiaoDien.Designer.cs
+++ QLthuvien/GUI/frmGiaoDien.Designer.cs
@@ -28,19 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gunaCircleButton1 = new Guna.UI.WinForms.GunaCircleButton();
+            this.gunaCircleButton2 = new Guna.UI.WinForms.GunaCircleButton();
+            this.gunaCircleButton3 = new Guna.UI.WinForms.GunaCircleButton();
             this.SuspendLayout();
+            // 
+            // gunaCircleButton1
+            // 
+            this.gunaCircleButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaCircleButton1.AnimationSpeed = 0.03F;
+            this.gunaCircleButton1.BaseColor = System.Drawing.Color.White;
+            this.gunaCircleButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.BorderSize = 2;
+            this.gunaCircleButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaCircleButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaCircleButton1.ForeColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.Image = null;
+            this.gunaCircleButton1.ImageSize = new System.Drawing.Size(52, 52);
+            this.gunaCircleButton1.Location = new System.Drawing.Point(143, 205);
+            this.gunaCircleButton1.Name = "gunaCircleButton1";
+            this.gunaCircleButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.gunaCircleButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaCircleButton1.OnHoverImage = null;
+            this.gunaCircleButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.Size = new System.Drawing.Size(200, 200);
+            this.gunaCircleButton1.TabIndex = 0;
+            this.gunaCircleButton1.Text = "Sách";
+            // 
+            // gunaCircleButton2
+            // 
+            this.gunaCircleButton2.AnimationHoverSpeed = 0.07F;
+            this.gunaCircleButton2.AnimationSpeed = 0.03F;
+            this.gunaCircleButton2.BaseColor = System.Drawing.Color.White;
+            this.gunaCircleButton2.BorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton2.BorderSize = 2;
+            this.gunaCircleButton2.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaCircleButton2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaCircleButton2.ForeColor = System.Drawing.Color.Black;
+            this.gunaCircleButton2.Image = null;
+            this.gunaCircleButton2.ImageSize = new System.Drawing.Size(52, 52);
+            this.gunaCircleButton2.Location = new System.Drawing.Point(501, 205);
+            this.gunaCircleButton2.Name = "gunaCircleButton2";
+            this.gunaCircleButton2.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.gunaCircleButton2.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton2.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaCircleButton2.OnHoverImage = null;
+            this.gunaCircleButton2.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaCircleButton2.Size = new System.Drawing.Size(200, 200);
+            this.gunaCircleButton2.TabIndex = 1;
+            this.gunaCircleButton2.Text = "User";
+            // 
+            // gunaCircleButton3
+            // 
+            this.gunaCircleButton3.AnimationHoverSpeed = 0.07F;
+            this.gunaCircleButton3.AnimationSpeed = 0.03F;
+            this.gunaCircleButton3.BaseColor = System.Drawing.Color.White;
+            this.gunaCircleButton3.BorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton3.BorderSize = 2;
+            this.gunaCircleButton3.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaCircleButton3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaCircleButton3.ForeColor = System.Drawing.Color.Black;
+            this.gunaCircleButton3.Image = null;
+            this.gunaCircleButton3.ImageSize = new System.Drawing.Size(52, 52);
+            this.gunaCircleButton3.Location = new System.Drawing.Point(859, 205);
+            this.gunaCircleButton3.Name = "gunaCircleButton3";
+            this.gunaCircleButton3.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.gunaCircleButton3.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton3.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaCircleButton3.OnHoverImage = null;
+            this.gunaCircleButton3.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaCircleButton3.Size = new System.Drawing.Size(200, 200);
+            this.gunaCircleButton3.TabIndex = 2;
+            this.gunaCircleButton3.Text = "Mượn trả";
             // 
             // frmGiaoDien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(910, 479);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1213, 590);
+            this.Controls.Add(this.gunaCircleButton3);
+            this.Controls.Add(this.gunaCircleButton2);
+            this.Controls.Add(this.gunaCircleButton1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmGiaoDien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmGiaoDien";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI.WinForms.GunaCircleButton gunaCircleButton1;
+        private Guna.UI.WinForms.GunaCircleButton gunaCircleButton2;
+        private Guna.UI.WinForms.GunaCircleButton gunaCircleButton3;
     }
 }
