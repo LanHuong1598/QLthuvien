7bee6a9abaababdb8ecc315056bbe34e78c4d940
khanhtoan99gvc <khanhtoan99gvc@gmail.com>
2019-10-10 15:16:14 +0700

    form muon sach

--- QLthuvien/GUI/FormMuonSach.cs
+++ QLthuvien/GUI/FormMuonSach.cs
@@ -52,11 +52,11 @@ namespace QLthuvien.GUI
         private void InitializeComponent()
         {
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txt_MaDG = new Guna.UI.WinForms.GunaTextBox();
             this.cb_Cuonsach = new System.Windows.Forms.ComboBox();
+            this.txt_MaDG = new Guna.UI.WinForms.GunaTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.gunaPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -72,32 +72,13 @@ namespace QLthuvien.GUI
             this.gunaPanel1.Size = new System.Drawing.Size(611, 370);
             this.gunaPanel1.TabIndex = 0;
             // 
-            // label1
+            // cb_Cuonsach
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(62, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 71);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(65, 135);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.cb_Cuonsach.FormattingEnabled = true;
+            this.cb_Cuonsach.Location = new System.Drawing.Point(159, 82);
+            this.cb_Cuonsach.Name = "cb_Cuonsach";
+            this.cb_Cuonsach.Size = new System.Drawing.Size(160, 24);
+            this.cb_Cuonsach.TabIndex = 4;
             // 
             // txt_MaDG
             // 
@@ -115,13 +96,32 @@ namespace QLthuvien.GUI
             this.txt_MaDG.TabIndex = 3;
             this.txt_MaDG.Text = "gunaTextBox1";
             // 
-            // cb_Cuonsach
+            // label3
             // 
-            this.cb_Cuonsach.FormattingEnabled = true;
-            this.cb_Cuonsach.Location = new System.Drawing.Point(159, 72);
-            this.cb_Cuonsach.Name = "cb_Cuonsach";
-            this.cb_Cuonsach.Size = new System.Drawing.Size(160, 24);
-            this.cb_Cuonsach.TabIndex = 4;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // FormMuonSach
             // 
